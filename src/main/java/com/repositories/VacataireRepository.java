b13cc954ccfdd8816488c15b9b836baab6b1bd12
Yuna <80405048+yunaoc@users.noreply.github.com>
2022-03-08 10:40:39 +0100

    Delete src directory

--- src/main/java/com/repositories/VacataireRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repositories;
-
-import com.entities.Vacataire;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VacataireRepository extends JpaRepository<Vacataire, Long> {
-}
