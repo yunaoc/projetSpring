11934c8f88a473e7db240d6ada6871a6587b8545
Yuna <80405048+yunaoc@users.noreply.github.com>
2022-03-08 11:06:49 +0100

    Delete ComposanteRepository.java

--- src/main/java/com/repositories/ComposanteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repositories;
-
-import com.entities.Composante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComposanteRepository extends JpaRepository<Composante, Long> {
-}
