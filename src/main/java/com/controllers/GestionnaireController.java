b3fabdc84bf91f543239c4183740fc6e747721fe
Yuna <80405048+yunaoc@users.noreply.github.com>
2022-03-08 11:50:32 +0100

    Add files via upload

---
+++ src/main/java/com/controllers/GestionnaireController.java
@@ -0,0 +1,56 @@
+package com.controllers;
+
+import com.dtos.GestionnaireDto;
+import com.services.impl.GestionnaireServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gestionnaire")
+public class GestionnaireController {
+
+    private final GestionnaireServiceImpl gestionnaireService;
+
+    public GestionnaireController(GestionnaireServiceImpl gestionnaireService) {this.gestionnaireService = gestionnaireService;}
+
+    /**
+     * <p>Get all gestionnaires in the system</p>
+     * @return List<GestionnaresDto>
+     */
+    @GetMapping
+    public List<GestionnaireDto> getGestionnaires() {
+        return gestionnaireService.getAllGestionnaire();
+    }
+
+    /**
+     * Method to get the gestionnaire based on the ID
+     */
+    @GetMapping("/{id}")
+    public GestionnaireDto getGestionnaire(@PathVariable Long id){return gestionnaireService.getGestionnaireById(id);}
+
+    /**
+     * Create a new Gestionnaire in the system with idComposante
+     */
+    @PostMapping("/{id}")
+    public GestionnaireDto createGestionnaire(final @RequestBody GestionnaireDto gestionnaireDto, @PathVariable Long id){
+        return gestionnaireService.createGestionnaire(gestionnaireDto, id);
+    }
+
+    /**
+     * Update a Gestionnaire in the system
+     */
+    @PutMapping("/{id}")
+    public GestionnaireDto updateGestionnaire(final @RequestBody GestionnaireDto gestionnaireDto, @PathVariable Long id){
+        return gestionnaireService.updateGestionnaire(gestionnaireDto,id);
+    }
+
+    /**
+     * Delete a gestionnaire by its id
+     */
+    @DeleteMapping("/{id}")
+    public Boolean deleteGestionnaire(@PathVariable Long id){
+        return gestionnaireService.deleteGestionnaire(id);
+    }
+
+}
