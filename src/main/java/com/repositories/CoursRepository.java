60cec846ac98fa9edc787d0df41616136d721bb2
Yuna <80405048+yunaoc@users.noreply.github.com>
2022-03-08 11:06:57 +0100

    Delete CoursRepository.java

--- src/main/java/com/repositories/CoursRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repositories;
-
-import com.entities.Cours;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoursRepository extends JpaRepository<Cours, Long> {
-}
